b43a2fb3a3014238090d43e5ad0538343e38a6df
Hayashibara <create.manage.mov@gmail.com>
2025-03-14 20:18:21 +0900

    a

--- Cランクレベルアップメニュー/forループ/Step4.js
+++ Cランクレベルアップメニュー/forループ/Step4.js
@@ -1,1 +1,22 @@
-let answer = sLines.map(item => item.indexOf(cLines.map(citem => citem))> 0 ? "YES" : "NO");
+// 2
+// c
+// d
+// 2
+// cat
+// dog
+
+const main = (input) => {
+    
+    const lines = input.trim().split("\n");
+    const n = Number(lines[0]);
+    const m = lines[n + 1];
+    const sLines = lines.slice(n + 2);
+    const cLines = lines.slice(1,n + 1);
+
+    let answer = sLines.map(item => item.indexOf(cLines.map(citem => citem))> 0 ? "YES" : "NO");
+
+    //answer.forEach(item => console.log(item));
+    
+}
+
+main(require("fs").readFileSync("/dev/stdin","utf-8"));
