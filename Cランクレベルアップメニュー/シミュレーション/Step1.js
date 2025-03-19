bebc0de889705865cc4d8fbc526d4f6a3d99aa9d
Hayashibara <create.manage.mov@gmail.com>
2025-03-19 13:05:37 +0900

    Step1.js completed

---
+++ Cランクレベルアップメニュー/シミュレーション/Step1.js
@@ -0,0 +1,9 @@
+const main = (input) => {
+    
+    //const inputArr = input.trim().split("\n");
+    console.log(Math.ceil(10000 / 13) * 13);
+    
+
+}
+
+main(require("fs").readFileSync("/dev/stdin","utf-8"));
