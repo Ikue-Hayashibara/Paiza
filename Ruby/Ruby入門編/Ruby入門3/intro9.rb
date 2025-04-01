e9f14c0c14f2fccdd809e44270a7f41250bf13aa
Hayashibara <create.manage.mov@gmail.com>
2025-04-01 23:14:49 +0900

    Ruby入門2,3 clear

---
+++ Ruby/Ruby入門編/Ruby入門3/intro9.rb
@@ -0,0 +1,12 @@
+# 特定期間の西暦年と昭和年の対応表を作る
+# 1行目：開始年
+# 2行目：期間
+# 昭和年 = 西暦年 - 1925
+# 出力：西暦XXXX年は、昭和YY年です。
+
+seireki = gets.to_i
+years = gets.to_i
+for i in seireki..seireki + years -1
+    showa = i - 1925
+    puts "西暦#{i}年は、昭和#{showa}年です。"
+end
