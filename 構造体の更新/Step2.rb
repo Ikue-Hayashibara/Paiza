90ebad2e74377abe338bb571793a558fbbdacab3
Hayashibara <create.manage.mov@gmail.com>
2025-04-17 22:39:44 +0900

    updata

--- 構造体の更新/Step2.rb
+++
@@ -1,8 +0,0 @@
-n = gets.chomp.to_i
-user_data = n.times.map {gets.chomp.split(" ")}
-k = gets.chomp
-Student = Struct.new(:name, :old, :birth, :state)
-user_data.each do |data|
-    user = Student.new(*data)
-    if user.old == k then puts user.name end
-end
