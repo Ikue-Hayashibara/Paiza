39d42d63824b57822a0f0d2eb4e3dda11dfe693e
Hayashibara <create.manage.mov@gmail.com>
2025-04-17 22:03:42 +0900

    Step3.rb clear

---
+++ 構造体の更新/Step3.rb
@@ -0,0 +1,11 @@
+n = gets.chomp.to_i
+user_data = n.times.map {gets.chomp.split(" ")}
+Student = Struct.new(:name, :old, :birth, :state)
+user_struct_arr = []
+user_data.each do |data|
+    user = Student.new(*data)
+    user_struct_arr << user
+end
+
+arr = user_struct_arr.sort_by { |u| u.old }
+arr.each { |a| puts a.to_a.join(" ") }
